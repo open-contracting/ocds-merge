73f3c62a9ddf05afbfb04c8ae6ea977018564f87
James McKinney <james@slashpoundbang.com>
2018-11-26 15:02:12 -0500

    Use importlib. Fix Travis.

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 from glob import glob
 
@@ -8,16 +9,12 @@ from tests import fixtures
 def test_all():
     schema_url = 'http://standard.open-contracting.org/schema/1__1__2/release-schema.json'
 
-    filenames = glob(os.path.join('tests', 'fixtures', '*.py'))
+    filenames = glob(os.path.join('tests', 'fixtures', '*_example.py'))
     assert len(filenames), 'fixtures not found'
     for filename in filenames:
-        name = filename.split('/')[-1].split('.')[0]
-        if name == "__init__":
-            continue
+        basename = os.path.splitext(os.path.basename(filename))[0]
+        fixture = importlib.import_module('tests.fixtures.' + basename)
 
-        __import__("tests.fixtures." + name)
-        fixture = getattr(fixtures, name)
-
-        assert ocdsmerge.merge(fixture.releases) == fixture.compiledRelease, '{} compiledRelease differs'.format(name)
-        assert ocdsmerge.merge(fixture.releases, schema_url) == fixture.compiledRelease, '{} compiledRelease with schema differs'.format(name)
-        assert ocdsmerge.merge_versioned(fixture.releases) == fixture.versionedRelease, '{} versionedRelease differs'.format(name)
+        assert ocdsmerge.merge(fixture.releases) == fixture.compiledRelease, '{} merge differs'.format(basename)
+        assert ocdsmerge.merge(fixture.releases, schema_url) == fixture.compiledRelease, '{} merge with schema differs'.format(basename)  # noqa
+        assert ocdsmerge.merge_versioned(fixture.releases) == fixture.versionedRelease, '{} merge_versioned differs'.format(basename)  # noqa
