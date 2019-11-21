847fa126fca58c0ebfb1e1eb4376e7cfecfff25c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-21 18:44:50 -0500

    Fix variable shadowing in test_fixtures.py

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -21,14 +21,14 @@ from jsonschema.validators import Draft4Validator as validator
 
 from tests import path, load, tags
 
-schema_path = 'release-schema-{}.json'
+release_schema_path = 'release-schema-{}.json'
 versioned_release_schema_path = 'versioned-release-validation-schema-{}.json'
 
 test_valid_argvalues = []
 for minor_version, patch_tag in tags.items():
-    filenames = glob(path(minor_version, '*.json'))
+    filenames = glob(path(os.path.join(minor_version, '*.json')))
     assert len(filenames), 'ocds fixtures not found'
-    for versioned, schema_path in ((False, schema_path), (True, versioned_release_schema_path)):
+    for versioned, schema_path in ((False, release_schema_path), (True, versioned_release_schema_path)):
         schema = load(schema_path.format(patch_tag))
         for filename in filenames:
             if not versioned ^ filename.endswith('-versioned.json'):
