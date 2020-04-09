4095d0c29d9a0b80b9cda698bf1e693a8819d203
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-09 16:17:42 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,9 @@ setup(
     },
     classifiers=[
         'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
