453fcaad0247c2054008c891ddd3421fcbc12d2d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-10-02 14:47:58 -0400

    Remove separators argument (it is already set to this value if indent is not None)

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -54,7 +54,7 @@ def test_valid(filename, schema):
     for datum in data:
         for error in validator(schema, format_checker=FormatChecker()).iter_errors(datum):
             errors += 1
-            warnings.warn(json.dumps(error.instance, indent=2, separators=(',', ': ')))
+            warnings.warn(json.dumps(error.instance, indent=2))
             warnings.warn('{} ({})\n'.format(error.message, '/'.join(error.absolute_schema_path)))
 
     assert errors == 0, '{} is invalid. See warnings below.'.format(filename)
