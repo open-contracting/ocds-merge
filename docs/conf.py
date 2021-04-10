8c3025159f1ea3f988a7846f19e7c7e01fa435d9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-04-10 17:43:32 -0400

    ci: Add Python wheels distribution

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2019, Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The short X.Y version
-version = '0.6.5'
+version = '0.6.6'
 # The full version, including alpha/beta/rc tags
 release = version
 
