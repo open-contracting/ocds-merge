5bfb181c1dfedd33946aeefb846e0ef54fe0a613
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-14 22:27:47 -0400

    chore: raise from e, so that the user can know in what context sorted_releases is called

--- ocdsmerge/util.py
+++ ocdsmerge/util.py
@@ -41,26 +41,26 @@ def sorted_releases(releases: list[dict[str, Any]]) -> list[dict[str, Any]]:
         return releases
     try:
         return sorted(releases, key=lambda release: release['date'])
-    except KeyError:
-        raise MissingDateKeyError('date', 'The `date` field of at least one release is missing.') from None
+    except KeyError as e:
+        raise MissingDateKeyError('date', 'The `date` field of at least one release is missing.') from e
     except TypeError as e:
         if ' not supported between instances of ' in e.args[0]:
             if 'NoneType' in e.args[0]:
-                raise NullDateValueError('The `date` field of at least one release is null.') from None
-            raise NonStringDateValueError('The `date` field of at least one release is not a string.') from None
+                raise NullDateValueError('The `date` field of at least one release is null.') from e
+            raise NonStringDateValueError('The `date` field of at least one release is not a string.') from e
         if e.args[0] in ('string indices must be integers', "string indices must be integers, not 'str'",
                            'string index indices must be integers or slices, not str'):
             raise NonObjectReleaseError(
                 'At least one release is a string, not a dict. Use `json.loads` to parse the string as JSON.'
-            ) from None
+            ) from e
         if e.args[0] == 'byte indices must be integers or slices, not str':
             raise NonObjectReleaseError(
                 'At least one release is a byte-string, not a dict. Use `json.loads` to parse the byte-string as JSON.'
-            ) from None
+            ) from e
         if e.args[0] == 'list indices must be integers or slices, not str':
-            raise NonObjectReleaseError('At least one release is a list, not a dict.') from None
+            raise NonObjectReleaseError('At least one release is a list, not a dict.') from e
         if e.args[0] == 'tuple indices must be integers or slices, not str':
-            raise NonObjectReleaseError('At least one release is a tuple, not a dict.') from None
+            raise NonObjectReleaseError('At least one release is a tuple, not a dict.') from e
         if e.args[0] in ("'set' object is not subscriptable", "'set' object is not subscriptable (key 'date')"):
-            raise NonObjectReleaseError('At least one release is a set, not a dict.') from None
+            raise NonObjectReleaseError('At least one release is a set, not a dict.') from e
         raise
