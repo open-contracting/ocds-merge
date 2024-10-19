62b561e790148fe5b4408d534d1e344323bd46f0
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-10-18 23:29:38 -0400

    chore: Never use relative import. Use __all__ instead of noqa.

--- ocdsmerge/__init__.py
+++ ocdsmerge/__init__.py
@@ -1,2 +1,10 @@
-from .flatten import APPEND, MERGE_BY_POSITION  # noqa: F401
-from .merge import CompiledRelease, Merger, VersionedRelease  # noqa: F401
+from ocdsmerge.flatten import APPEND, MERGE_BY_POSITION
+from ocdsmerge.merge import CompiledRelease, Merger, VersionedRelease
+
+__all__ = (
+    "APPEND",
+    "MERGE_BY_POSITION",
+    "CompiledRelease",
+    "Merger",
+    "VersionedRelease",
+)
